69b4f02b6f095eb727c0c3bffb752e87826a6ab5
Ahmed Jafri <ajafri19@gmail.com>
2015-01-29 03:17:18 -0800

    replace deprecated sendfile function with sendFile

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, '/build')));
 require('./routes/contact')(app);
 
 app.get('/*', function(req, res) {
-        res.sendfile(path.join(__dirname, '/build/index.html'));
+        res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
 server.listen(app.get('port'), function() {
