e86a497fe322a2b02bc7ed073e619f5f48912f09
Ahmed Jafri <ajafri19@gmail.com>
2015-04-12 17:50:45 -0700

    [angular-soundcloud-player] Cache tracks on first load

--- public/js/angular-soundcloud-player.js
+++ public/js/angular-soundcloud-player.js
@@ -3,64 +3,69 @@
 require('angular/angular');
 
 var player = angular.module('angular-soundcloud-player', []);
+var cachedTracks = undefined;
 
 player.directive('soundcloudPlayer', function() {
     return {
-    	scope: true,
-    	link: function link(scope, element, attrs) {
-    		scope.tracks = [];
+        controller: function($scope, $attrs) {
+            if(cachedTracks) {
+                $scope.tracks = cachedTracks;
+            } else
+            {
+                $scope.tracks = [];
 
-    		var resourceLink = attrs.soundcloudPlayer;
-    		if(!$.trim(resourceLink))
-    			throw new Error("resource link not set on directive soundcloud-player. Ex. <div soundcloud-player=\"/users/4246445/favorites\"></div>");
-			SC.initialize({
-		    	client_id: "YOUR_CLIENT_ID"
-		  	});
-		  	SC.get(resourceLink, {limit: 100}, function(tracks){
-		  		var _tracks = [];
+                var resourceLink = $attrs.soundcloudPlayer;
+                if(!$.trim(resourceLink))
+                    throw new Error("resource link not set on directive soundcloud-player. Ex. <div soundcloud-player=\"/users/4246445/favorites\"></div>");
+                SC.initialize({
+                    client_id: "YOUR_CLIENT_ID"
+                });
+                SC.get(resourceLink, {limit: 100}, function(tracks){
+                    var _tracks = [];
 
-		  		for(var i = 0; i < tracks.length; i++) {
-		  			if(!tracks[i].artwork_url)
-		  				tracks[i].artwork_url = "https://a-v2.sndcdn.com/assets/images/default/cloudx200-38b02b00.png";
+                    for(var i = 0; i < tracks.length; i++) {
+                        if(!tracks[i].artwork_url)
+                            tracks[i].artwork_url = "https://a-v2.sndcdn.com/assets/images/default/cloudx200-38b02b00.png";
 
-		  			if(tracks[i].streamable)
-		  				_tracks.push(tracks[i]);
-		  		}
-		  		scope.$apply(function(){
-					scope.tracks = _tracks;
-			    });
-			});
-        },
-        controller: function($scope) {
-            $scope.playTrack = function(trackURI) {
-            	SC.stream(trackURI, {
+                        if(tracks[i].streamable)
+                            _tracks.push(tracks[i]);
+                    }
+
+                    cachedTracks = _tracks;
+
+                    $scope.$apply(function(){
+                        $scope.tracks = _tracks;
+                    });
+                });
+            }
+
+            $scope.playTrack = function(track) {
+                if($scope.player && $scope.currentTrack.id == track.id) {
+                    $scope.player.togglePause();
+                    return;
+                }
+
+            	SC.stream(track.uri, {
 					useHTML5Audio: true
             	}, function(sound){
-            		if($scope.currentTrack)
-            			$scope.currentTrack.stop();
+            		if($scope.player)
+            			$scope.player.stop();
 
-            		$scope.currentTrack = sound;
-					sound.play();
+                    $scope.player = sound;
+                    $scope.currentTrack = track;
+
+					$scope.player.play();
 				});
             };
+
+            $scope.getIcon = function(track) {
+                if($scope.currentTrack && $scope.currentTrack.id == track.id && !$scope.player.paused)
+                    return "uk-icon-pause"
+                else
+                    return "uk-icon-play"
+            }
         }
     };
 });
-
-//checks if flash is installed/enabled on the browser
-function isFlashEnabled()
-{
-    var hasFlash = false;
-    try
-    {
-        var fo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
-        if(fo) hasFlash = true;
-    }
-    catch(e)
-    {
-        if(navigator.mimeTypes ["application/x-shockwave-flash"] != undefined) hasFlash = true;
-    }
-    return hasFlash;
-}
 
 module.exports = player;
