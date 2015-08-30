d8fb8813682d6dea4f29f031ddfcc8a70841090b
Ahmed Jafri <ajafri19@gmail.com>
2015-08-29 20:04:21 -0700

    Add support for emailing from contact form

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,16 @@
 'use strict';
 
+var nodemailer = require('nodemailer');
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.EMAIL_U,
+        pass: process.env.EMAIL_P
+    }
+});
+
 
 module.exports = function(app) {
 
@@ -10,9 +21,27 @@ module.exports = function(app) {
   		if(req.body.text.length <= 0)
   			return next(new Error("Contact body not set"));
 
-  		var err = new Error("Not implemented yet.");
-  		err.status = 500;
-  		return next(err);
+      // NB! No need to recreate the transporter object. You can use
+      // the same transporter object for all e-mails
+
+      // setup e-mail data with unicode symbols
+      var mailOptions = {
+          from: 'Ahmed\'s Website <blahblah10898@gmail.com>', // sender address
+          to: 'ajafri19@gmail.com', // list of receivers
+          subject: req.body.email + ' contacted you', // Subject line
+          text: req.body.text
+      };
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, function(error, info){
+          if(error){
+            var err = new Error("Not implemented yet.");
+            err.status = 500;
+            return next(err);
+          }else{
+              res.status(200).send();
+          }
+      });
   });
 
 	function validateEmail(email) {
